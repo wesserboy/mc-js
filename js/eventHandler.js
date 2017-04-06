f4d0db32c79471ff33302d639b0427ba845b8e4e
wesserboy <wesleywillekes@MacBook-Pro-van-wesley.local>
2017-04-07 01:59:52 +0200

    Added support for resizing the window
    The canvas used to take up a little bit more space than the size of the
    window, this has been fixed using the added css code in page.html.
    
    Resizing the window now resizes the canvas with it and redraws the
    scene. As a result the canvas will always take up the whole window, and
    the image will look as it's supposed to, even during/after a window
    resize.

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -1,6 +1,8 @@
 var onLoad = function(){
 	gl = initWebGL(document.getElementById("gl-canvas"));
 
+	window.addEventListener("resize", onResize);
+
 	if(!gl){
 		return;
 	}
@@ -10,7 +12,19 @@ var onLoad = function(){
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
-    
+
+    drawScene();
+};
+
+var onResize = function(){
+	var canvas = document.getElementById("gl-canvas");
+
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+
     drawScene();
 };
 
